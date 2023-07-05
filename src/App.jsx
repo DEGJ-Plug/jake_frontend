b5a75851f168844453799d78e0a0a7dcc4c08198
Warmarth <godsonibeaka@gmail.com>
2023-07-05 14:10:21 +0100

    navbar nd  signin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import "./index.css";
+
 function App() {
   return (
-    <header className=" bg-stone-700 ">
+    <header className="navbar bg-stone-700 font-mono">
       {/* logo */}
+      <article className="logo">
+        <h3>
+          <span>jake</span>plug
+        </h3>
+      </article>
       {/* <!-- nav bar --> */}
-      <nav className="flex sm:justify-center  space-x-4">
+      <nav className="flex sm:justify-center space-x-4">
         {[
           ["Sign-up", "/sign-up"],
           ["Home", "/home"],
@@ -14,12 +21,29 @@ function App() {
         ].map(([title, url]) => (
           <a
             href={url}
-            className="rounded-lg px-2 py-3 text-slate-200 font-medium hover:bg-slate-100 hover:text-slate-900"
+            className="rounded-lg px-2 py-3 text-slate-200 font-medium hover:border-slate-100 hover:text-slate-900"
           >
             {title}
           </a>
         ))}
       </nav>
+      <div>
+        <form action="" className="mx-1">
+          <input
+            type="search"
+            className="search bg-sky-500/75 px-2 py-2"
+            name="search"
+            placeholder="search"
+            id=""
+          />
+          <button
+            type="submit"
+            className="px-1 py-2 rounded bg-cyan-500 hover:bg-cyan-600"
+          >
+            search
+          </button>
+        </form>{" "}
+      </div>
     </header>
   );
 }
--- src/user.jsx
+++ src/user.jsx
@@ -1,12 +1,15 @@
+
 function user() {
   return (
     <section className="user-section">
       <article className="user-details">
         <div>
+          <img src={require()} alt="" />
+        </div>
+        <div>
           <h3>username</h3>
           <h3>email</h3>
         </div>
-        
       </article>
     </section>
   );
