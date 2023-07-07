69b4b8d2a8ab146dd14303fbcebf002da6b7e578
Warmarth <godsonibeaka@gmail.com>
2023-07-07 06:18:49 +0100

    categories , header-icons, and reponsiveness  done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,24 @@
 import { NavLink, Link } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { RiCustomerService2Fill } from "react-icons/ri";
+import {
+  MdOutlineProductionQuantityLimits,
+  MdOutlineMiscellaneousServices,
+} from "react-icons/md";
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center mx-12 my-3">
-      <div className="text-4xl font-bold">
-        <span className="text-blue-600">J</span>
-        <span className="text-gray-400">a</span>
+      <div className="text-4xl font-bold animate-bounce ">
+        <span className="text-blue-600 ">J</span>
+        <span className="text-gray-400 ">a</span>
         <span className="text-blue-600">K</span>
-        <span className="text-gray-400">e</span>
+        <span className="text-gray-400 animate-pulse">e</span>
       </div>
-      <div className="font-normal text-xl">
-        <ul className="flex">
-          <li className="mx-3 hover:text-blue-600 hover:scale-125">
+      <div className="font-normal flex justify-betweem text-xl">
+        <ul className="flex transition-all">
+          <FaHome className="md:hidden mx-4 hover:text-blue-600 hover:scale-125" />
+          <li className="mx-3 hidden md:flex hover:text-blue-600 hover:scale-125 transition-all">
             <NavLink
               to="/"
               className={({ isActive }) => isActive && "text-blue-600"}
@@ -19,30 +26,39 @@ const Header = () => {
               Home
             </NavLink>
           </li>
-          <li className="mx-3 hover:text-blue-600 hover:scale-125">
-            <NavLink
-              to="/products"
-              className={({ isActive }) => isActive && "text-blue-600"}
-            >
-              Products
-            </NavLink>
-          </li>
-          <li className="mx-3 hover:text-blue-600 hover:scale-125">
-            <NavLink
-              to="/services"
-              className={({ isActive }) => isActive && "text-blue-600"}
-            >
-              Services
-            </NavLink>
-          </li>
-          <li className="mx-3 hover:text-blue-600 hover:scale-125">
-            <NavLink
-              to="/FAQ"
-              className={({ isActive }) => isActive && "text-blue-600"}
-            >
-              FAQ
-            </NavLink>
-          </li>
+          <ul className="flex transition-all">
+            <MdOutlineProductionQuantityLimits className="md:hidden mx-4 hover:text-blue-600 hover:scale-125" />
+            <li className=" mx-3 hidden md:flex hover:text-blue-600 hover:scale-125 transition-all">
+              <NavLink
+                to="/products"
+                className={({ isActive }) => isActive && "text-blue-600"}
+              >
+                Products
+              </NavLink>
+            </li>
+          </ul>
+          <ul className="flex transition-all">
+            <MdOutlineMiscellaneousServices className="md:hidden mx-4 hover:text-blue-600 hover:scale-125" />
+            <li className="mx-3 hidden md:flex hover:text-blue-600 hover:scale-125 transition-all">
+              <NavLink
+                to="/services"
+                className={({ isActive }) => isActive && "text-blue-600"}
+              >
+                Services
+              </NavLink>
+            </li>
+          </ul>
+          <ul className="flex transition-all">
+            <RiCustomerService2Fill className="md:hidden mx-4 hover:text-blue-600 hover:scale-125" />
+            <li className="mx-3 hidden md:flex hover:text-blue-600 hover:scale-125 transition-all">
+              <NavLink
+                to="/FAQ"
+                className={({ isActive }) => isActive && "text-blue-600"}
+              >
+                FAQ
+              </NavLink>
+            </li>
+          </ul>
         </ul>
       </div>
       <div className="font-normal text-lg">
--- src/components/SearchProduct.jsx
+++ src/components/SearchProduct.jsx
@@ -2,8 +2,8 @@ import { FcSearch } from "react-icons/fc";
 import headphone_pic from "../assets/headphone_pic.jpg";
 const SearchProduct = () => {
   return (
-    <section className="bg-blue-600 flex items-center justify-between mx-16 my-16 rounded-lg">
-      <div className="ml-20 w-3/6">
+    <section className="bg-blue-600 flex h-72 items-center justify-between mx-16 my-16 rounded-lg">
+      <div className="ml-20 w-3/6 min-w-fit">
         <h1 className="text-white font-bold text-2xl mb-6">
           Get the best product at <br /> your home
         </h1>
@@ -16,8 +16,12 @@ const SearchProduct = () => {
           <FcSearch className="text-4xl cursor-pointer hover:scale-125" />
         </div>
       </div>
-      <div>
-        <img className="m-4" src={headphone_pic} alt="headphone img" />
+      <div className="hidden sm:hidden md:flex ">
+        <img
+          className="m-4 rounded-lg "
+          src={headphone_pic}
+          alt="headphone img"
+        />
       </div>
     </section>
   );
